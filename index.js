13521b8821ae82c3e8b94804e0460e07d50f3b88
SarkerFardinFeroz <sarkerfardinferoz1@gmail.com>
2023-11-09 19:20:52 +0600

    Implemented Jwt verify token methood

--- index.js
+++ index.js
@@ -26,13 +26,69 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// middleware
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log({ token });
+
+  //no token available
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
     const categoryCollection = client.db("jobNest").collection("category");
     const jobsCollection = client.db("jobNest").collection("jobs");
- 
+    const appliedCollection = client.db("jobNest").collection("appliedJob");
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/applied", verifyToken, async (req, res) => {
+      try {
+        const cursor = appliedCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    app.post("/applied", async (req, res) => {
+      try {
+        const appliedData = req.body;
+        const result = await appliedCollection.insertOne(appliedData);
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
+    });
 
     app.put("/apply/:id", async (req, res) => {
       try {
@@ -51,26 +107,45 @@ async function run() {
     });
 
     app.get("/category", async (req, res) => {
-      const cursor = categoryCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
+      try {
+        const cursor = categoryCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+      }
     });
     app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+      try{
+        const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+      }
+      catch (error) {
+        console.log(error);
+      }
     });
     app.get("/job-details/:id", async (req, res) => {
-      const id = req.params.id;
+      try{
+        const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
+      }
+      catch (error) {
+        console.log(error);
+      }
     });
 
     app.post("/jobs", async (req, res) => {
-      const body = req.body;
+      try{
+        const body = req.body;
       const result = await jobsCollection.insertOne(body);
       res.send(result);
+      }
+      catch (error) {
+        console.log(error);
+      }
     });
 
     // delete job
@@ -115,13 +190,18 @@ async function run() {
     });
 
     // get my jobs
-    app.get("/my-jobs", async (req, res) => {
+    app.get("/my-jobs", verifyToken, async (req, res) => {
+    try{
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
       }
       const result = await jobsCollection.find(query).toArray();
       res.send(result);
+    }
+      catch (error) {
+        console.log(error);
+      }
     });
 
     // Send a ping to confirm a successful connection
